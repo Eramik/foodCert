c2b69afd621cd6c778370150faac7cbb77d50b2c
Denis <erami.ukr@gmail.com>
2020-11-05 05:21:09 +0200

    Added getting locales

--- src/endpoints/userData.js
+++ src/endpoints/userData.js
@@ -1,9 +1,10 @@
 import Transportation from '../models/Transportation';
 import logger from '../utils/Logger';
 import getAuthedUser from '../utils/getAuthedUser';
+import locales from '../../config/locales';
 
 export default (app) => {
-  app.get('/getMyTransportations/', async (req, res) => {
+  app.get('/getMyTransportations', async (req, res) => {
     try {
       const user = await getAuthedUser(req.query.authToken);
       if (!user) {
@@ -12,6 +13,14 @@ export default (app) => {
       
       const myTrasportations = await Transportation.find({ $or: [{ providerId: user._id }, { clientId: user._id }, { transporterId: user._id }] }).lean();
       return res.json({ transportations: myTrasportations });
+    } catch (e) {
+      logger.error(e);
+      return res.status(500).json({ error: e });
+    }
+  });
+  app.get('/getlocales', async (req, res) => {
+    try {
+      return res.json({ locales });
     } catch (e) {
       logger.error(e);
       return res.status(500).json({ error: e });
