36e845104cd60d2323396c9dc2121ebf9b484aa5
Denis <erami.ukr@gmail.com>
2020-12-24 00:31:24 +0200

    Added endpoint to receive data from smart device

--- src/endpoints/temperatureMapReceiver.js
+++ src/endpoints/temperatureMapReceiver.js
@@ -3,6 +3,7 @@ const logger = require('../utils/Logger');
 const getAuthedSmartDeviceOwner = require('../utils/getAuthedSmartDeviceOwner');
 const { getQualityScore } = require('../logic/analyseTemperatureMaps');
 const { generateCertificate } = require('../logic/certificateGeneration');
+const User = require('../models/User');
 
 module.exports = (app) => {
   app.post('/temperatureMap/:transportationId', async (req, res) => {
@@ -38,7 +39,37 @@ module.exports = (app) => {
       
       return res.json({ success: true });
     } catch (e) {
-      logger.error(e);
+      console.log(e);
+      return res.status(500).json({ error: e });
+    }
+  });
+
+  app.post('/transportationFromDevice', async (req, res) => {
+    try {
+      const dataReceivedAt = new Date().valueOf();
+      const data = req.body;
+      let transportation = data.transportation;
+
+      const user = await User.findOne({ smartDeviceTokens: data.device_token }).lean();
+
+      transportation.transporterId = user._id;
+      transportation.minimalAllowedTemperature = 20;
+      transportation.maximalAllowedTemperature = 22;
+      const lastTemperatureMap = transportation.temperatureMaps[transportation.temperatureMaps.length - 1];
+      const measurmentStartedAt = dataReceivedAt - lastTemperatureMap.rawCreationTimestamp;
+      transportation.temperatureMaps.forEach(map => {
+        map.creationTimestamp = new Date(measurmentStartedAt + map.rawCreationTimestamp).toUTCString();
+      });
+
+      transportation = await Transportation.create(transportation);
+      const score = getQualityScore(transportation);
+      const certPath = await generateCertificate(transportation);
+      transportation.certificatePath = certPath;
+      transportation.score = score;
+      await transportation.save();      
+      return res.json({ success: true });
+    } catch (e) {
+      console.log(e);
       return res.status(500).json({ error: e });
     }
   });
